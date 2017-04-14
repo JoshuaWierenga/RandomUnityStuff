89578095e7344077431622aa8b6eaf4e4ec5f60b
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2017-04-14 17:52:09 +1000

    Import existing stuff
    unity tutorials

---
+++ Tutorials/Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Boundary")
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            if (other.tag == "Player")
+            {
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            }
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
